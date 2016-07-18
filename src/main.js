38399aeb8ef9b85d56c8f44fb6103509a75d4e70
Joey Melnick <jmelnick86@gmail.com>
2016-07-18 13:58:56 -0400

    nav bar opacity tweak

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('.headerMain');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.headerMain').css('background-color', 'rgba(34,34,34,0.9)');
+       } else {
+          $('.headerMain').css('background-color', 'transparent');
+       }
+   });
+});   
+                  
 $(document).ready(function(){
-
 $("#rotatingSection > div:gt(0)").hide();
 
 setInterval(function() {
