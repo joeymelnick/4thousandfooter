7d9ae2f433c30e986cf9e613c123c90ac9c02c25
Joey Melnick <jmelnick86@gmail.com>
2016-07-29 13:37:22 -0400

    added comments

--- src/js/controllers/mainController.js
+++ src/js/controllers/mainController.js
@@ -1,7 +1,6 @@
 app.controller('mainController', ['$scope', '$http', function($scope, $http) {
 var mountainArray = [];
-    // Takes two seperate arrays of mountain names and heights
-    // and turns it into an object.
+
    $http({
     url: 'https://api.myjson.com/bins/3c4yj',
     method: 'GET'
@@ -9,24 +8,28 @@ var mountainArray = [];
 mountainArray = response.data;
 
   
-//sets up mountains for ng-repeat
 
-    console.log(mountainArray);
+/*sort type coding*/
     $scope.sortType = 'identifier';
-    $scope.reverse = false
+    $scope.reverse = false;
+    
+/*this function calls an if statement that either changes the reverse variable or 
+sets it to false. it then sets sort type to the argument that is passed  */  
     $scope.sortBy = function(sortType){
       $scope.reverse = ($scope.sortType === sortType) ? !$scope.reverse : false;
-      console.log($scope.sortType)
-      $scope.sortType = sortType
-      console.log(sortType);
+      $scope.sortType = sortType;
     }
+    
+    /*sets the active mountain so you can compare it on the click directive*/
     $scope.activeMountain = '';
         $scope.setMountain = function(x){
               $scope.activeMountain = x;
-      
         };            
+    
+    /*sets array for repeat */
     $scope.mountains = mountainArray;
 
+/*    shows the info page for the cooresponding mountain*/
     $scope.thisMountain= function(obj){
                 return $scope.activeMountain === obj;
         };
